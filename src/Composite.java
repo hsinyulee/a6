4c9cfa8099bb670097d5f514ca0572e43c61dc27
hsinyulee <kngngop@gmail.com>
2015-12-21 16:44:06 +0800

    ver.1

---
+++ src/Composite.java
@@ -0,0 +1,18 @@
+public class Composite {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	class Question {
+		   String description;
+		   public Question(String desc) {
+		      this.description = desc;
+		   }
+		   public void print() {
+		      System.out.println(desc);
+		   }      
+		}
+}
